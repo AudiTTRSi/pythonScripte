f4fa998d0c7624691a9140e00154ff1baa79dcec
AudiTTRSi <rokkepa@protonmail.com>
2022-05-14 09:58:15 +0200

    adding cheetsheet this part is on lists methods

---
+++ PythonCheatSheet.py
@@ -0,0 +1,18 @@
+# Lists methods 101
+
+example_list = [1, 2, 3, 4]
+
+# Using Append
+example_list.append(5)
+# print(example_list)
+
+# Using Remove
+example_list.remove(5)
+# print(example_list)
+
+# adding more items
+example_list_2=example_list + [11, 22, 33]
+# adding more items
+broken_prices = [5, 3, 4, 5, 4] + [4]
+
+# Accessing List Elements
