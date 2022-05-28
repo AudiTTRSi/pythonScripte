5f115e3c606e6299969e9a1b6dd1c8f584f52ccb
AudiTTRSi <rokkepa@protonmail.com>
2022-05-28 08:47:27 +0200

    adding part on list comprehension

--- PythonCheatSheet.py
+++ PythonCheatSheet.py
@@ -16,3 +16,15 @@ example_list_2=example_list + [11, 22, 33]
 broken_prices = [5, 3, 4, 5, 4] + [4]
 
 # Accessing List Elements
+
+#####################
+#list comprehension #
+#####################
+
+grades = [90, 88, 62, 76, 74, 89, 48, 57]
+#all values her are +10 to what it is in grades list
+scaled_grades = [grade+10 for grade in grades]
+
+heights = [161, 164, 156, 144, 158, 170, 163, 163, 157]
+#heights that are biggr than 161
+can_ride_coaster = [height for height in heights if height > 161]
