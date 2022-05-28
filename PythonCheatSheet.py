0de8e9bc86508bf5e3ca2417a688959bbd751886
AudiTTRSi <rokkepa@protonmail.com>
2022-05-28 11:57:54 +0200

    pythocheatsheet added thing on list comprehensions

--- PythonCheatSheet.py
+++ PythonCheatSheet.py
@@ -21,10 +21,20 @@ broken_prices = [5, 3, 4, 5, 4] + [4]
 #list comprehension #
 #####################
 
+#example 1
 grades = [90, 88, 62, 76, 74, 89, 48, 57]
 #all values her are +10 to what it is in grades list
 scaled_grades = [grade+10 for grade in grades]
 
+#example 2
 heights = [161, 164, 156, 144, 158, 170, 163, 163, 157]
 #heights that are biggr than 161
 can_ride_coaster = [height for height in heights if height > 161]
+
+#example 3
+single_digits = list(range(10))
+cubes = [digit**3 for digit in single_digits]
+
+#####################
+#  functions        #
+#####################
