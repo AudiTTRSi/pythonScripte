f95c68b168cb62345cae4eb6a564302e1b64f107
AudiTTRSi <rokkepa@protonmail.com>
2022-06-04 18:38:05 +0200

    added some more string methods

--- PythonCheatSheet.py
+++ PythonCheatSheet.py
@@ -62,3 +62,31 @@ reapers_line_one = " ".join(reapers_line_one_words)
 
 winter_trees_lines = ['All the complicated details', 'of the attiring and', 'the disattiring are completed!', 'A liquid moon', 'moves gently among', 'the long branches.', 'Thus having prepared their buds', 'against a sure winter', 'the wise trees', 'stand sleeping in the cold.']
 winter_trees_full = '\n'.join(winter_trees_lines)
+
+#strip ()
+love_maybe_lines = ['Always    ', '     in the middle of our bloodiest battles  ', 'you lay down your arms', '           like flowering mines    ','\n' ,'   to conquer me home.    ']
+
+love_maybe_lines_stripped=[]
+for i in love_maybe_lines:
+  stripped = i.strip()
+  love_maybe_lines_stripped.append(stripped)
+
+print(love_maybe_lines_stripped)
+love_maybe_full = "\n".join(love_maybe_lines_stripped)
+print(love_maybe_full)
+
+#replace
+toomer_bio = \
+"""
+Nathan Pinchback Tomer, who adopted the name Jean Tomer early in his literary career,
+was born in Washington, D.C. in 1894. Jean is the son of Nathan Tomer was a mixed-race freedman,
+born into slavery in 1839 in Chatham County, North Carolina.
+Jean Tomer is most well known for his first book Cane,
+which vividly portrays the life of African-Americans in southern farmlands.
+"""
+
+toomer_bio_fixed = toomer_bio.replace('Tomer', 'Toomer')
+
+#find
+god_wills_it_line_one = "The very earth will disown you"
+disown_placement = god_wills_it_line_one.find("disown")
